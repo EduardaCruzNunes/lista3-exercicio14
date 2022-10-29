a2e49c5d9e9ab951b441d81f163d2bca8e4399c2
unknown <Eduarda Nunes>
2022-10-28 23:09:11 -0300

    exercicio-14

---
+++ script.js
@@ -0,0 +1,15 @@
+var valor = 1;
+var contador = 1;
+var numero = 0;
+
+function calcularOfatorial(numero){//8
+    while(contador <= numero){
+        console.log(valor , '*', contador)
+        valor = valor * contador;
+        contador = contador + 1;
+    }
+    return console.log(valor)
+}
+numero = prompt('Digite um número:')
+
+calcularOfatorial(numero);
